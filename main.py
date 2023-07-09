3c00531a0c60140d9e974c4c0da459ad8e9e3106
David Maizil <davidmaizil@Davids-MBP.ultrahub>
2023-07-09 21:58:47 +0100

    added filtering to show only python jobs

--- main.py
+++ main.py
@@ -12,14 +12,21 @@ results = soup.find(id="ResultsContainer")
 # find all elements with a class name
 job_elements = results.find_all("div", class_="card-content")
 
+# finds jobs that has python in the text
+python_jobs = results.find_all(
+    "h2", string=lambda text: "python" in text.lower()
+)
 
-# print(results.prettify()) // create indentations and make the html more readable
+# iterate over python jobs 3 generations up to find all the info about them
+python_jobs_element = [
+    h2_element.parent.parent.parent for h2_element in python_jobs
+]
 
-for job_element in job_elements:
+for job_element in python_jobs_element:
     title_element = job_element.find("h2", class_="title")
     company_element = job_element.find("h3", class_="company")
     location_element = job_element.find("p", class_="location")
-    print(title_element)
-    print(company_element)
-    print(location_element)
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
     print()
